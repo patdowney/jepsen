9720c992f6fc696f00ee881d9c98cf0389b7490a
Korny Sietsma <korny@sietsma.com>
2016-05-05 10:02:32 +0100

    removed hacky timestamp adding, now calculate timestamps from nanoseconds in post-processing

--- mongodb-fiddle/src/jepsen/mongodb/core.clj
+++ mongodb-fiddle/src/jepsen/mongodb/core.clj
@@ -3,7 +3,7 @@
                      [string :as str]]
             [clojure.java.io :as io]
             [clojure.pprint :refer [pprint]]
-            [clojure.tools.logging :refer [debug info warn trace]]
+            [clojure.tools.logging :refer [debug info warn trace spy]]
             [clojure.walk :as walk]
             [jepsen [core      :as jepsen]
                     [db        :as db]
@@ -333,6 +333,26 @@
        (catch com.mongodb.MongoSocketReadTimeoutException e#
          (assoc ~op :type error-type# :error :socket-read)))))
 
+(def nanos-offset "this server's approximate offset between nanoTime and epoch time in ms"
+  (let [ctms (System/currentTimeMillis)
+        nt (System/nanoTime)]
+    (- (* ctms 1000000) nt)))
+
+(defn nanos->epochms "convert a time from nanoTime to system ms" [nanos]
+  (if nanos
+    (if-let [rto util/*relative-time-origin*]
+      (long (/ (+ nanos nanos-offset rto) 1000000))
+      (throw (ex-info "Can't calculate epoch times if relative-time-origin isn't set" {})))))
+
+(defn fix-ts [history-row]
+  (let [start-nanos (:time history-row)
+        complete-nanos (:time (:completion history-row))]
+    (as-> history-row h
+        (assoc h :timestamp (nanos->epochms start-nanos))
+        (if complete-nanos
+          (assoc-in h [:completion :timestamp] (nanos->epochms complete-nanos))
+          h))))
+
 (defn perf-dump
   "Spits out performance stats"
   []
@@ -340,10 +360,11 @@
     (check [_ test model history opts]
       (if-let [perfdumpfile (:perfdumpfile test)]
         (let [filename (store/path! test (:subdirectory opts)
-                                    perfdumpfile)]
+                                    perfdumpfile)
+              fixed-history (map fix-ts (util/history->latencies history))]
           (info "running custom history dump to " filename)
           (with-open [out (io/writer filename)]
-            (cheshire/generate-stream (util/history->latencies history) out {:pretty true}))))
+            (cheshire/generate-stream fixed-history out {:pretty true}))))
       {:valid? true})))
 
 (defn checkers [] (checker/compose {:perf-dump (perf-dump)
