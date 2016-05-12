301ff8dc5b30c9467773587ec1f4eb37797f367f
Korny Sietsma <korny@sietsma.com>
2016-05-12 20:50:19 +0100

    lower mongo logs a bit

--- mongodb-fiddle/resources/logback.groovy
+++ mongodb-fiddle/resources/logback.groovy
@@ -99,5 +99,5 @@ root(TRACE, ["FILE_ASYNC","STDOUT_FILTERED"])
 
 // specify log levels, and optionally other appenders, for specific namespaces
 logger("jepsen", TRACE)
-logger("org.mongodb", INFO) // mongo is pretty verbose - trim it down
+logger("org.mongodb", WARN) // mongo is pretty verbose - trim it down
 logger("stash", INFO, ["STASH"])
